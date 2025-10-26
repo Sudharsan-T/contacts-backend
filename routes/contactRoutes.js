96e6a878835903fe6c2271890ad75ccebc01a56d
SUDHARSAN_T <sudharsant44@gmail.com>
2025-10-26 23:20:10 +0530

    setup errorHandler

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -9,14 +9,8 @@ const {
 } = require("../controllers/contactController");
 
 
-router.route("/").get(getContacts).post;
+router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
-router.route("/").post(createContact);
-
-router.route("/:id").get(getContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
 
 module.exports = router;
